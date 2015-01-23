2d76aec739c59d8f81dc4ebdf358fc2aaa9b92db
stone1652 <erickson.nathen@gmail.com>
2015-01-22 18:02:45 -0700

    removed extra function

--- usernamefilearray.js
+++ usernamefilearray.js
@@ -1,11 +1,7 @@
 function UserListObject (userName) {
-    this.userName = userName;
+	this.userName = userName;
 	this.imageList = [];
 	
-	
-    this.getInfo = function() {
-        return this.color + ' ' + this.type + ' apple';
-    };
 	this.getNext = function() {
 		
 	};
